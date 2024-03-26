ed88f337968b57dc1c85b7db72b570aa40ea4380
YashR461 <rastogiyash461@gmail.com>
2024-03-26 20:25:39 +0530

    datatypes in solidity

---
+++ Basics/dataTypes.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract dataTypes {
+    uint256 public num1 = 256;
+    uint8 public num2 = uint8(num1);
+
+    bool public yash = true;
+}
